bed17404cde1938c24c2ab40025ba4237d5a9b63
tianshikun <a88wangtian@163.com>
2017-04-24 13:55:06 +0800

    #trait.md Trait.php

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
-PHP学习笔记
-生命不止,学习不老!
-引用的网站有
-
-www.coding.xin
-
+PHP学习笔记<br>
+生命不止,学习不老!<br>
+引用的网站有<br>
+www.coding.xin<br>
 www.symfony.cn
+
+About Basic:记录一些PHP中比较重要的和不常见的概念
+一个是知识点,一个是代码示例文件,文件名一般表示文件的内容，比如<br>
+trait.md表示trait的相关内容,Trait.php则是相关的代码
--- basic/Trait.php
+++ basic/Trait.php
@@ -11,7 +11,10 @@ trait ezcReflectionReturnInfo
     { /*1*/
         return "this is second trait";
     }
-    
+    /*public function sayHello()
+    {
+        echo 'hello';
+    }*/
     function getReturnDescription()
     { /*2*/
     }
@@ -41,21 +44,72 @@ trait SayWorld
 {
     public function sayHello()
     {
-        parent::sayHello();
         echo 'World!';
     }
 }
 
 class MyHelloWorld extends Base
 {
-    use SayWorld,ezcReflectionReturnInfo;
+    /*use SayWorld,ezcReflectionReturnInfo{
+        ezcReflectionReturnInfo::sayHello insteadof SayWorld;
+        SayWorld::sayHello as say;
+    }*/
 
     public function sayHello()
     {
         echo 'the is begin:';
     }
+
 }
 
-$o = new MyHelloWorld();
+/*$o = new MyHelloWorld();
 $o->sayHello();
-$o->getReturnType();
+$o->getReturnType();*/
+//冲突以及解决方法
+//Fatal error: Trait method sayHello has not been applied, because there are collisions with other trait methods on MyHelloWorld in E:\php-learning\basic\Trait.php on line 59
+//修改方法的访问控制
+class controllerClass extends Base {
+    /*use SayWorld,ezcReflectionReturnInfo {
+        SayWorld::sayHello as private;
+        ezcReflectionReturnInfo::getReturnType as public;
+    }*/
+}
+/*$ex = new controllerClass();
+$ex->getReturnType();*/
+trait baseTrait {
+    use SayWorld,ezcReflectionReturnInfo;
+}
+class baseTraitTestClass {
+    use baseTrait;
+}
+
+//抽象方法的使用
+trait instanceTrait {
+    public function getType(){
+        echo "trait";
+    }
+    abstract public function getData();
+}
+
+class testInstanceTrait {
+    use instanceTrait;
+    public function getData(){
+        echo "is end";
+    }
+}
+$ex = new testInstanceTrait();
+$ex->getType();
+$ex->getData();
+//Trait 的静态成员
+trait staticMember{
+    static $c = 4;
+    public static function get(){
+        echo "this is static method in trait";
+    }
+}
+class testTraitStatic{
+    use staticMember;
+}
+$ex = new testTraitStatic();
+echo staticMember::$c;
+testTraitStatic::get();
